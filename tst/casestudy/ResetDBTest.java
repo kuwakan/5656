6e1600318de8fe169eda582a7b19e49a41c0ca41
T-User <T-User@T-PC05>
2016-12-06 10:47:53 +0900

    微修正

--- tst/casestudy/ResetDBTest.java
+++ tst/casestudy/ResetDBTest.java
@@ -17,23 +17,11 @@ public class ResetDBTest {
 			con = ConnectionManager.getConnection();
 			System.out.println("接続完了");
 
-//			ResetDB resetDB = new ResetDB(con);
-//			resetDB.resetDB();
-//			System.out.println("全テーブルリセット完了");
+			ResetDB resetDB = new ResetDB(con);
+			resetDB.resetDB();
+			System.out.println("全テーブルリセット完了");
 
 		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-
-		try {
-			DBWriter dbWriter = new DBWriter(con);
-			// 引数1をファイル名、引数２をSQL文として扱う
-			dbWriter.createCSV("callsign");
-		}
-		catch (Exception e) {
-			// 何らかのエラーがあっても、表示するのみ
-			System.out.println("エラーです");
 			e.printStackTrace();
 		}
 

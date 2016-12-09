968c52edf4a4adb76c36a06e90bc4bc6dfc996e6
T-User <T-User@T-PC05>
2016-12-09 14:09:49 +0900

    RealDataTest修正

--- tst/casestudy/RealDataTest.java
+++ tst/casestudy/RealDataTest.java
@@ -1,7 +1,10 @@
 package casestudy;
 
+import static org.hamcrest.MatcherAssert.*;
+
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+
 
 public class RealDataTest {
 
@@ -9,23 +12,28 @@ public class RealDataTest {
 	@Test
 	public void SBS3を使用したテスト() {
 
+		RealData rd = new RealData()
 
-		RealData rd = new RealData(){
+//		{
+//			public boolean exec = false;
+//			@Override
+//			public void run(){
+//				exec = false;
+//				super.run();
+//				exec = true;
+//			}
+//		}
 
-			public boolean exec = false;
+		;
 
-			@Override
-			public void run(){
-				exec = true;
-				super.run();
-				exec = false;
-			}
-		};
-
+		/** rdを開始 */
 		rd.start();
-//		assertThat("");
 
+		/**	rd.run() の無限ループを抜けるためにdoinをfalseにする*/
 		rd.doin = false;
+
+		/** 無限ループ実行後にrd.doinはfalseとなるので、!rd.doinがtrueのとき処理は正常に実行されている */
+		assertThat("無限ループ実行後", !rd.doin);
 
 	}
 
